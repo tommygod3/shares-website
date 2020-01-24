eb9c980cb65df2a73a8a930405e4f62586ee414d
tommygod3 <tommygod1@live.com>
2020-01-24 11:08:14 +0000

    Removed time in news because of bug

--- shares-website/src/app/top-bar/news.service.ts
+++ shares-website/src/app/top-bar/news.service.ts
@@ -13,7 +13,7 @@ export class NewsService {
 
   constructor(private http: HttpClient) { }
 
-  private apiURL = 'https://newsapi.org/v2/everything?q=MSFT&from=2019-12-21&sortBy=publishedAt&apiKey=dc7a3451f097418cbdb125a4af9d3f73&language=en';
+  private apiURL = 'https://newsapi.org/v2/everything?q=MSFT&sortBy=publishedAt&apiKey=dc7a3451f097418cbdb125a4af9d3f73&language=en';
 
     get(): Observable<News> {
       return this.http.get<News>(this.apiURL);
